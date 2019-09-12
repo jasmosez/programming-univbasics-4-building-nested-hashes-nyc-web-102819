bb0e4a114aad24f8a695779620f089a453d1b7b8
James Schaffer <jas.schaffer@gmail.com>
2019-09-12 01:13:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
       },
     :names => {
       :reading_railroad => {},
-      :second => {},
+      :pennsylvania_railroad => {},
       :third => {},
       :fourth => {}
       }
